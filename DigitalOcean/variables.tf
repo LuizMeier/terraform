31772ef35a50b7696924474e228e7464f8006626
Luiz Meier <lfmmeier@gmail.com>
2023-09-05 17:22:12 -0300

    aula-2.5

---
+++ DigitalOcean/variables.tf
@@ -0,0 +1,18 @@
+variable "droplet_name" {
+  default = "vm-aula"
+}
+
+variable "droplet_region" {
+  default = "nyc1"
+}
+
+variable "droplet_size" {
+  default = "s-1vcpu-512mb-10gb"
+}
+
+variable "ssh_key_name" {
+  default = "WSL Terraform"
+}
+
+variable "do_token" {
+}
