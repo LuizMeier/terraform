5744900f6e7a267d385979beee586a7104e049cb
Luiz Meier <lfmmeier@gmail.com>
2023-09-10 18:08:24 -0300

    Workspace

---
+++ Workspace/dev.tfvars
@@ -0,0 +1,6 @@
+k8s_region ="nyc1"
+k8s_name = "k8s-dev"
+k8s_version = "1.27.4-do.0"
+k8s_node_size = "s-1vcpu-2gb"
+k8s_node_count = 1
+k8s_node_pool_name = "pool-teste"
--- Workspace/main.tf
+++ Workspace/main.tf
@@ -1,15 +1,76 @@
-resource "local_file" "arquivo" {
-  content  = var.conteudo
-  filename = "arquivo-${terraform.workspace}.txt"
-}
-
-variable "conteudo" {
-  type        = string
-  description = "Conteúdo do arquivo"
-}
+#resource "local_file" "arquivo" {
+#  content  = var.conteudo
+#  filename = "arquivo-${terraform.workspace}.txt"
+#}
+#
+#variable "conteudo" {
+#  type        = string
+#  description = "Conteúdo do arquivo"
+#}
 
 #variable "nome_arquivo" {
 #  type        = string
 #  description = "Conteúdo do arquivo"
 #}
 
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_kubernetes_cluster" "k8s" {
+  name    = "${var.k8s_name}-${terraform.workspace}"
+  region  = var.k8s_region
+  version = var.k8s_version
+
+  node_pool {
+    name       = var.k8s_node_pool_name
+    size       = var.k8s_node_size
+    node_count = var.k8s_node_count
+  }
+#  lifecycle {
+#    prevent_destroy = true
+#  }
+}
+
+variable "do_token" {
+  type = string
+}
+
+variable "k8s_region" {
+  type    = string
+  default = "nyc1"
+}
+
+variable "k8s_name" {
+  type    = string
+  default = "k8s-1-27-4-do-0-nyc1-1694377503795"
+}
+
+variable "k8s_version" {
+  type    = string
+  default = "1.27.4-do.0"
+}
+
+variable "k8s_node_size" {
+  type    = string
+  default = "s-1vcpu-2gb"
+}
+
+variable "k8s_node_count" {
+  type    = number
+  default = 1
+}
+
+variable "k8s_node_pool_name" {
+  type    = string
+  default = "pool-teste"
+}
